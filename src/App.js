fef34c849786f6d7e43c4a7b4a6f0a9853c242f7
ArthurToledo <arthur.toledo741@gmail.com>
2020-06-15 00:16:28 -0300

    Include modal functions and new styles button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Lottie from 'react-lottie';
+import { FiXCircle } from 'react-icons/fi';
 
 import './App.css';
 
@@ -10,15 +11,18 @@ import sadPeople from './assets/sadPeople.json';
 import programming_animation from './assets/programming-animation.json';
 import logoWhatsapp from './assets/logoWpp.png';
 import logoInstagram from './assets/logoInsta.png';
+import alert_notification from './assets/alert-notification.json';
 
 function App() {
+  const [modalActive, setModalActive] = useState(false);
+
   return (
     <div className="container">
       <div className="homeView">
         <header>
           <img src={logo} alt="Logo meu portófolio" />
 
-          <button>Quero meu portfólio</button>
+          <button onClick={() => setModalActive(true)}>Quero meu portfólio</button>
         </header>
 
         <div className="titlePage">
@@ -63,7 +67,7 @@ function App() {
 
             <h3 className="titleBenefitsRight">Pode ser compartilhado facilmente</h3>
             <p className="textBenefitsRight">A vantagem de um portfólio digital é a facilidade de compartilhá-lo seja via Linkedin, e-mail, etc., para os recrutadores e possíveis parceiros de negócios.</p>
-          
+
             <h3 className="titleBenefitsLeft">Mais cedo ou mais tarde seu trabalho será encontrado</h3>
             <p className="textBenefitsLeft">Se você gosta do que faz entenda que o portfólio é uma das melhores formas de alguém encontrar e confiar a você um trabalho.</p>
 
@@ -77,8 +81,8 @@ function App() {
         <div className="informationsQuest">
           <div className="textInformationsQuest">
             <h4>Quem somos nos</h4>
-            <p>A <strong>Meu Portfólio</strong> nasceu com o intuito de ajudar pessoas que estão em busca de recolocação ou estão querendo entrar no mercado de trabalho durante esse momento de pandemia. 
-              <br></br><br></br>Temos em nosso DNA muito mais do que criatividade, é predominante da nossa cultura entregar muito além de um portfólio para nossos clientes, o nosso objetivo é possibilitar 
+            <p>A <strong>Meu Portfólio</strong> nasceu com o intuito de ajudar pessoas que estão em busca de recolocação ou estão querendo entrar no mercado de trabalho durante esse momento de pandemia.
+              <br></br><br></br>Temos em nosso DNA muito mais do que criatividade, é predominante da nossa cultura entregar muito além de um portfólio para nossos clientes, o nosso objetivo é possibilitar
               que vocês possam reescrever suas histórias profissionais.
             </p>
           </div>
@@ -89,11 +93,34 @@ function App() {
 
       <footer>
         <div className="divImgsFooter">
-          <img src={logoWhatsapp} alt="Imagem para contato do whatsapp" />
-          <img src={logoInstagram} alt="Imagem para contato do instagram" />
+          <a href="https://api.whatsapp.com/send?phone=5531985800803&text=Oi%2C%20vi%20sua%20p%C3%A1gina%20%C3%A0%20respeito%20dos%20portf%C3%B3lios%20e%20gostaria%20de%20saber%20mais%20sobre" target="no_blank">
+            <img src={logoWhatsapp} alt="Imagem para contato do whatsapp" />
+          </a>
+          <a href="https://www.instagram.com/meuportfolio_digital/?hl=pt-br" target="no_blank">
+            <img src={logoInstagram} alt="Imagem para contato do instagram" />
+          </a>
         </div>
         &copy; Copyright 2020 Meu Portfólio
       </footer>
+
+      {modalActive &&
+        <div className="modalButtonClick">
+          <div className="modal">
+            <button onClick={() => setModalActive(false)}>
+              <FiXCircle size={40} color="#4D4D4D" />
+            </button>
+            <Lottie options={{ loop: true, autoplay: true, animationData: alert_notification }} height={'50%'} width={'50%'} />
+
+            <div className="informationsModal">
+              <h1>Não temos vagas no momento</h1>
+              <p>Infelizmente nossa agenda está cheia no momento, siga nossas redes sociais ou entre em contato via whatsapp 
+                conosco para ficar atento quando abrirmos novas vagas para que você possa garantir seu <strong>portfólio</strong> feito 
+                pela <strong>Meu Portfólio</strong>.
+              </p>
+            </div>
+          </div>
+        </div>
+      }
     </div>
   );
 }
